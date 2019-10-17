e74e1c558ac3901b7b5e1dd7975b54f7ce28a862
snowaye <saungaye7@gmail.com>
2019-10-17 23:29:24 +0630

    Setup Project

---
+++ app/src/main/java/com/padc/batch9/assignment10/MovieApplicatiion.kt
@@ -0,0 +1,10 @@
+package com.padc.batch9.assignment10
+
+import android.app.Application
+
+class MovieApplicatiion: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/padc/batch9/assignment10/data/model/BaseModel.kt
@@ -0,0 +1,14 @@
+package com.padc.batch9.assignment10.data.model
+
+import android.content.Context
+
+
+abstract class BaseModel {
+//    protected val dataAgent:EventDataAgent = RetroFitAgent
+//
+//    protected lateinit var database: EventDatabase
+//
+//    fun initDatabase(context: Context){
+//        database = EventDatabase.getInstance(context)
+//    }
+}
